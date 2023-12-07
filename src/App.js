0f9f5932b5c05aa26fcb2e1ffe99b12d0d836809
Rockgowtham <rockgowtham1207@gmail.com>
2023-12-07 07:38:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        <strong> Name:</strong> ROCKGOWTHAM N<br />
+        <strong> Department:</strong> Computer Science And Engineering<br />
+        <strong> Year:</strong> Third Year<br />
+        <strong> Register Number:</strong> 73772114187<br />
+        <strong> Area of Interest:</strong> Data Science<br />
+ 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
